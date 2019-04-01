3e8c96098d442c00fa3c26abb5c73a63ed071476
Agent of User <git@agentofuser.com>
2019-03-31 20:02:05 -0400

    Update bio and profile pic

--- src/components/bio.js
+++ src/components/bio.js
@@ -58,10 +58,10 @@ function Bio() {
               }}
             />
             <p>
-              Written by <strong>{author}</strong> who lives and works in San
-              Francisco building useful things.{' '}
+              Written by <strong>{author}</strong> who lives and works on 🌎
+              Earth building useful things.{' '}
               <a href={`https://twitter.com/${social.twitter}`}>
-                You should follow him on Twitter
+                You should follow them on Twitter
               </a>
             </p>
           </div>

87df07feef8bc5b751b2c943656f4dc91822d6d5
Agent of User <git@agentofuser.com>
2019-04-02 03:49:54 -0400

    Make deployment prettier with ora spinners

--- ipfs-deploy.js
+++ ipfs-deploy.js
@@ -3,6 +3,7 @@ const which = require('which')
 const clipboardy = require('clipboardy')
 const pinataSDK = require('@pinata/sdk')
 const updateCloudflareDnslink = require('dnslink-cloudflare')
+const ora = require('ora')
 
 require('dotenv').config()
 
@@ -10,6 +11,8 @@ async function updateDns(hash) {
   const key = process.env.CF_API_KEY
   const email = process.env.CF_API_EMAIL
   const domain = process.env.SITE_DOMAIN
+
+  const spinner = ora()
 
   if (!key || !email || !domain || !hash) {
     throw new Error('Missing information for updateDns()')
@@ -27,8 +30,10 @@ async function updateDns(hash) {
   }
 
   try {
+    spinner.info('📡 Beaming new hash to DNS provider...')
     const content = await updateCloudflareDnslink(api, opts)
-    console.log(`Updated TXT ${opts.record} to ${content}`)
+    spinner.succeed(`🙌 SUCCESS! Updated TXT ${opts.record} to ${content}.`)
+    spinner.succeed('🌐 Your website is deployed now :)')
   } catch (err) {
     console.log(err)
     process.exit(1)
@@ -42,12 +47,18 @@ async function main() {
 
   const df = IPFSFactory.create({ exec: ipfsBinAbsPath })
 
+  const spinner = ora()
+  spinner.start()
+  spinner.info('☎️Connecting to local IPFS daemon...')
+
   df.spawn({ disposable: false, init: false, start: false }, (err, ipfsd) => {
     if (err) throw err
 
     ipfsd.start([], (err2, ipfsClient) => {
       if (err2) throw err2
+      spinner.succeed('📶  Connected.')
 
+      spinner.info('💾 Adding and pinning ./public/ locally...')
       ipfsClient.addFromFs(
         'public',
         { recursive: true },
@@ -55,6 +66,7 @@ async function main() {
           if (err3) throw err3
 
           const { hash } = localPinResult[localPinResult.length - 1]
+          spinner.succeed(`#️⃣ Added locally as ${hash}.`)
 
           ipfsClient.id((err4, { addresses }) => {
             if (err4) throw err4
@@ -81,14 +93,14 @@ async function main() {
               process.env.PINATA_SECRET_API_KEY
             )
 
+            spinner.info('📠 Requesting remote pin to Pinata.cloud...')
             pinata
               .pinHashToIPFS(hash, pinataOptions)
-              .then(pinataPinResult => {
-                console.log("SUCCESS! It's pinned to Pinata now.")
-                console.log({ pinataPinResult })
+              .then(_pinataPinResult => {
+                spinner.succeed("📌 It's pinned to Pinata now.")
 
                 clipboardy.writeSync(hash)
-                console.log(`Root hash ${hash} copied to clipboard.`)
+                spinner.succeed(`📋 Hash ${hash} copied to clipboard.`)
 
                 updateDns(hash)
               })

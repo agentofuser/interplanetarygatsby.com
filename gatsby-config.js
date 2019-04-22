33ba2a25fd706f25038b5c027da92598dcb9f513
Agent of User <git@agentofuser.com>
2019-04-22 14:56:02 -0400

    Remove ganalytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,12 +50,6 @@ module.exports = {
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
-    {
       resolve: 'gatsby-plugin-feed',
       options: {
         query: `
